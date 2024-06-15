8163bc0c70196ff407dce27e37032922500a703d
ringo kwon <ringo.kwon@gmail.com>
2024-06-15 13:34:18 +0900

    parsing done

--- sec13_nodejs/app.js
+++ sec13_nodejs/app.js
@@ -1,12 +1,20 @@
-const http = require("http");
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>"+new Date().toISOString()+"</h1>");
-  } else if ((request.url === "/")) {
-    response.statusCode = 200;
-    response.end("<h1>Hello world!</h1>");
-  }
-}
-const server = http.createServer(handleRequest);
-server.listen(3000);
+const express = require("express");
+
+app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:3000/currenttime
+app.get("/", function (req, res) {
+  res.send(
+    "<form action ='/store-user' method='POST'><label>Your Name</label type='text'><input type='text', name='username'></input><button>Submit</button></from>"
+  );
+}); //localhost:3000/
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log(userName);
+  res.send("<h1>Username stored!</h1>");
+});
+app.listen(3000);
