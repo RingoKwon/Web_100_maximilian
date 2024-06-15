286e70bc431ef1376db8e3869a1b49aaf1e47069
ringo kwon <ringo.kwon@gmail.com>
2024-06-16 01:01:40 +0900

    include partial contents

--- sec19-static_dynamic_content_w_temp/app.js
+++ sec19-static_dynamic_content_w_temp/app.js
@@ -8,36 +8,42 @@ app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-res.render('index')
+  res.render("index");
 });
 app.get("/recommend", function (req, res) {
-    res.render('recommend')
+  res.render("recommend");
 });
 app.post("/recommend", function (req, res) {
-    const restaurant = req.body;
-    const filePath = path.join(__dirname, "data", "restaurant.json");
-  
-    const fileData = fs.readFileSync(filePath);
-    const storeRestaurants = JSON.parse(fileData);
-  
-    storeRestaurants.push(restaurant);
-  
-    fs.writeFileSync(filePath, JSON.stringify(storeRestaurants));
-  
-    res.redirect("/confirm");
-  });
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storeRestaurants = JSON.parse(fileData);
+
+  storeRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storeRestaurants));
+
+  res.redirect("/confirm");
+});
 app.get("/about", function (req, res) {
-    res.render('about')
+  res.render("about");
 });
 app.get("/confirm", function (req, res) {
-    res.render('confirm')
+  res.render("confirm");
 });
 app.get("/restaurants", function (req, res) {
-    res.render('restaurants')
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storeRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storeRestaurants.length,
+    restaurants: storeRestaurants,
+  });
 });
 app.listen(3000);
