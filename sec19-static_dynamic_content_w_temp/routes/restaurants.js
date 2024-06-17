a884c01c1061e880ed0c1c22abf952f1991f4d5d
ringo kwon <ringo.kwon@gmail.com>
2024-06-17 21:59:42 +0900

    query param

--- sec19-static_dynamic_content_w_temp/routes/restaurants.js
+++ sec19-static_dynamic_content_w_temp/routes/restaurants.js
@@ -23,18 +23,33 @@ router.get("/confirm", function (req, res) {
   e;
 });
 router.get("/restaurants", function (req, res) {
+  let order = req.query.order;
+  let nextOrder = "desc";
+
+  if (order !== "asc" && order !== "desc") {
+    order = "asc";
+  }
+
+  if (order === "desc") {
+    nextOrder = "asc";
+  }
+
   const storedRestaurants = resData.getStoredRestaurants();
 
   storedRestaurants.sort(function (resA, resB) {
-    if (resA.name > resB.name) {
-      return 1
+    if (
+      (order === "asc" && resA.name > resB.name) ||
+      (order === "desc" && resB.name > resA.name)
+    ) {
+      return 1;
     }
-    return -1
+    return -1;
   });
 
   res.render("restaurants", {
     numberOfRestaurants: storedRestaurants.length,
     restaurants: storedRestaurants,
+    nextOrder: nextOrder,
   });
 });
 
@@ -45,8 +60,8 @@ router.get("/restaurants/:id", function (req, res) {
     if (restaurant.id === restaurantId) {
       return res.render("restaurant-detail", { restaurant: restaurant });
     }
-}
-res.status(404).render("404");
+  }
+  res.status(404).render("404");
 });
 
 module.exports = router;
