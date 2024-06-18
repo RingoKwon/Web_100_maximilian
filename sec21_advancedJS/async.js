64ff3985fabe3f67271e91035764379613e9cf98
ringo kwon <ringo.kwon@gmail.com>
2024-06-18 14:45:03 +0900

    advancec

--- sec21_advancedJS/async.js
+++ sec21_advancedJS/async.js
@@ -1,14 +1,24 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
-function readFile() {
+async function readFile() {
   let fileData;
 
-fs.readFile("data.txt", function(error, fileData){
+  // fs.readFile("data.txt", function(error, fileData){
 
-    console.log('File parsing done!');
-    console.log(fileData.toString());
-});
+  //     console.log('File parsing done!');
+  //     console.log(fileData.toString());
+  // // start another async task that sends the data to a database
+  // });
+  try { 
 
+      fileData = await fs.readFile("data.txt");
+  } catch ( error) { 
+    console.log(error)
+  }
+  // .then(function (fileData) {
+  console.log("File parsing doen!");
+  console.log(fileData.toString());
+  // })
 
   console.log("Hi there!");
 }
