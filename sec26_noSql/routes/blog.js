a50ce795ad7d44a419b07a464c490aaf86718365
ringo kwon <ringo.kwon@gmail.com>
2024-06-22 16:44:09 +0900

    onging mongo

--- sec26_noSql/routes/blog.js
+++ sec26_noSql/routes/blog.js
@@ -1,28 +1,50 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('../data/database')
+const db = require("../data/database");
+const { ObjectId } = require("mongodb"); //class
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.redirect('/posts');
+router.get("/", function (req, res) {
+  res.redirect("/posts");
 });
 
-router.get('/posts', function(req, res) {
-  res.render('posts-list');
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    .toArray();
+  res.render("posts-list", { posts: posts });
 });
 
-router.get('/new-post', async function(req, res) {
-  const authors = await db.getDb().collection('authors').find().toArray() ; 
-  console.log(authors)
-  res.render('create-post',{authors : authors});
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  // console.log(authors); // deleted by ringo
+  res.render("create-post", { authors: authors });
 });
 
-router.post('/post', function(req, res) {
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author); //class
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
   const newPost = {
-    title: req.body.title , 
-    summary: req.body.summary, 
-    body: req.body.content
-  }
-})
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
+});
+
 module.exports = router;
