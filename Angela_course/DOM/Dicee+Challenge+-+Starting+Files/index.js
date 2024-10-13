4c5580d23232b9b974f61a04f5d8f99ba45bd42b
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-14 08:52:32 +0900

    done

--- Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
+++ Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
@@ -10,4 +10,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 
-image2.setAttribute("src", randomImageSource2)
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "player1 win";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "player2 win";
+} else {
+    document.querySelector("h1").innerHTML = "Draw"; 
+}
