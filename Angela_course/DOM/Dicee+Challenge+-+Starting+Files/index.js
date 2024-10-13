a10e7c4dc0656bd4d02ceb003684a901e39a1efa
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-14 08:48:00 +0900

    dice done

--- Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
+++ Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
@@ -1,7 +1,13 @@
 // Generate a random number between 1 and 6
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randommDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randommDiceImage;
+var image1 = document.querySelectorAll("img")[0];
 
+image1.setAttribute("src", randomImageSource);
 
-document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1];
 
-console.log(document.querySelector(".img1").getAttribute("src"));
+image2.setAttribute("src", randomImageSource2)
