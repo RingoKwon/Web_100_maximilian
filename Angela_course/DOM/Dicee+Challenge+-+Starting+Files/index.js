7114950e7a2e24efa50f8a41e3be2879724142a3
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-14 08:23:14 +0900

    link js file

--- Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
+++ Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
@@ -1,5 +1,7 @@
 // Generate a random number between 1 and 6
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-// Log the result to the console for testing
-console.log('randomNumber1:', randomNumber1);
+
+document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+
+console.log(document.querySelector(".img1").getAttribute("src"));
