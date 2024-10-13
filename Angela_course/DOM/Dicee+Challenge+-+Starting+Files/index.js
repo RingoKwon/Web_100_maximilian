9b59524a127d1edca626b81322a905aef196d705
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-14 08:05:42 +0900

    dice mySol set ranNum

---
+++ Angela_course/DOM/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,5 @@
+// Generate a random number between 1 and 6
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+// Log the result to the console for testing
+console.log('randomNumber1:', randomNumber1);
