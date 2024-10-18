df875bc5c8152e1e6266114005908f00e7e36574
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-18 14:29:41 +0900

    simonGame_ add button click event

--- Angela_course/Simon Game Challenge Starting Files/game.js
+++ Angela_course/Simon Game Challenge Starting Files/game.js
@@ -1,25 +1,31 @@
-var buttonColours = ["red", "blue", "green", "yellow"]; 
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomIndex = randomNumber(); 
+var randomIndex = randomNumber();
 var randomChoosenColor = buttonColours[randomIndex];
 
-function randomNumber(){
-    return Math.floor(Math.random() * 4); // 3을 4로 변경
+function randomNumber() {
+  return Math.floor(Math.random() * 4); // 3을 4로 변경
 }
 
-var gamePattern = []; 
-gamePattern.push(randomChoosenColor); 
+var gamePattern = [];
+gamePattern.push(randomChoosenColor);
 
-
-var audio = new Audio('sounds/'+randomChoosenColor + '.mp3');
+var audio = new Audio("sounds/" + randomChoosenColor + ".mp3");
 
 audio.play();
 
 setTimeout(function () {
-    $("#" + randomChoosenColor).addClass("pressed"); // 선택자에 #을 추가
-    
-    setTimeout(function() {
-        $("#" + randomChoosenColor).removeClass("pressed"); // 클래스 제거 추가
-    }, 100);
-    
+  $("#" + randomChoosenColor).addClass("pressed"); // 선택자에 #을 추가
+
+  setTimeout(function () {
+    $("#" + randomChoosenColor).removeClass("pressed"); // 클래스 제거 추가
+  }, 100);
 }, 100);
+
+// user choosen color
+var btnLen = document.querySelectorAll(".btn").length;
+for (let i = 0; i < btnLen; i++) {
+  document.querySelectorAll(".btn")[i].addEventListener("click", function () {
+    console.log(this.id);
+  });
+}
