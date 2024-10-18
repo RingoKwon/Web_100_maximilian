b96d07446499a58ace8545b8e22f3e47dcd10a79
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-10-18 15:28:14 +0900

    simonGame Code Refactoring

--- Angela_course/Simon Game Challenge Starting Files/game.js
+++ Angela_course/Simon Game Challenge Starting Files/game.js
@@ -1,37 +1,37 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomIndex = randomNumber();
-var randomChoosenColor = buttonColours[randomIndex];
+var gamePattern = [];
+
+var userClickPattern = [];
 
 function randomNumber() {
   return Math.floor(Math.random() * 4); // 3을 4로 변경
 }
-
-var gamePattern = [];
-gamePattern.push(randomChoosenColor);
-
-var audio = new Audio("sounds/" + randomChoosenColor + ".mp3");
-
-audio.play();
-
 // user choosen color
-var userChoosenColor;
-var userClickPattern = [];
 var btnLen = document.querySelectorAll(".btn").length;
 
 for (let i = 0; i < btnLen; i++) {
+  var userChoosenColor;
   document.querySelectorAll(".btn")[i].addEventListener("click", function () {
     userChoosenColor = this.id;
     userClickPattern.push(userChoosenColor);
   });
 }
-// pattor array
-
 // auto select animation
-setTimeout(function () {
-  $("#" + randomChoosenColor).addClass("pressed");
+function nextSequence() {
+  var randomIndex = randomNumber();
+  var randomChoosenColor = buttonColours[randomIndex];
+  gamePattern.push(randomChoosenColor);
 
   setTimeout(function () {
-    $("#" + randomChoosenColor).removeClass("pressed");
+    $("#" + randomChoosenColor).addClass("pressed");
+
+    setTimeout(function () {
+      $("#" + randomChoosenColor).removeClass("pressed");
+    }, 100);
   }, 100);
-}, 100);
+
+  // Audio play
+  var audio = new Audio("sounds/" + randomChoosenColor + ".mp3");
+  audio.play();
+}
