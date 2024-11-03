d9078dfe4df38632c18b9930c069b326998d8245
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-04 08:13:58 +0900

    save

--- Angela_course/JQeury/index.js
+++ Angela_course/JQeury/index.js
@@ -1,1 +1,9 @@
-$("h1").css("color", "red");
+$("h1").css("color", "blue");
+$("button").text("hey");
+$("button").text("<em>hey</em>");
+$("button").html("<em>hey</em>");
+$(".c2").css("color", "red")
+
+console.log($(".c1").hasclass(".c2"))
+
+
