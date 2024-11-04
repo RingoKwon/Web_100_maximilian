5b28fde78ab33bbae4bc1463cae9cf331ba2e591
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-04 13:18:03 +0900

    Solution Audio play

--- Angela_course/Simon Game Challenge Starting Files 2/game.js
+++ Angela_course/Simon Game Challenge Starting Files 2/game.js
@@ -3,6 +3,18 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  randomChosenColour = buttonColours[randomNumber];
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
+
+
+
+// for loop
+// for (var i = 0; i < gamePattern.length; i++) {
+//   $("#" + gamePattern[i]).css("background-color", "black");
+// }
