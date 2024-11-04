697a75359e5eb6354e51825dc99589beacc5608e
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-04 09:00:58 +0900

    save

--- Angela_course/JQeury/index.js
+++ Angela_course/JQeury/index.js
@@ -1,9 +1,24 @@
-$("h1").css("color", "blue");
+// $("h1").css("color", "blue");
 $("button").text("hey");
 $("button").text("<em>hey</em>");
 $("button").html("<em>hey</em>");
-$(".c2").css("color", "red")
+$(".c2").css("color", "red");
 
-console.log($(".c1").hasclass(".c2"))
+$("a").attr("href", "www.hh.com");
 
+$("h1").click(function () {
+  $("h1").css("color", "black");
+});
 
+$("button").click(function () {
+  $("h1").css("color", "yellow");
+});
+
+// 방법 1: input 이벤트 사용 (권장)
+
+// 또는
+
+// 방법 2: keydown 이벤트 사용
+$("input").keypress(function(event){
+    console.log(event.key);
+});
