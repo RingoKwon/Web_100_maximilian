f8e2e53012876d192525ace38cfcab04b9a930cc
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-04 12:26:28 +0900

    step2 mySol

--- Angela_course/Simon Game Challenge Starting Files 2/game.js
+++ Angela_course/Simon Game Challenge Starting Files 2/game.js
@@ -1,1 +1,13 @@
-alert('DD')
+var gamePattern = [];
+var randomNumber = nextSequence();
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  var randomN = Math.floor(Math.random() * 4);
+  return randomN;
+}
+
+randomChosenColour = buttonColours[randomNumber];
+gamePattern.push(randomChosenColour); 
+
+
