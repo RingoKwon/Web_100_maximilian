e02e9569234dde1f572525acb0aedf6305e09a61
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-04 13:34:37 +0900

    s4 mySolution

--- Angela_course/Simon Game Challenge Starting Files 2/game.js
+++ Angela_course/Simon Game Challenge Starting Files 2/game.js
@@ -1,18 +1,24 @@
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
+var userClickPattern = [];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
-  
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
+
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
 
-
+$(".btn").on("click", function () {
+  var clickedElement = $(this);
+  userClickPattern.push(clickedElement.attr("id"));
+});
 
 // for loop
 // for (var i = 0; i < gamePattern.length; i++) {
