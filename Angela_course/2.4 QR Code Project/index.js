203927527ff28f42bd0a8f757fb4aa45f8e0753d
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-06 12:41:27 +0900

    mysol

--- Angela_course/2.4 QR Code Project/index.js
+++ Angela_course/2.4 QR Code Project/index.js
@@ -5,6 +5,27 @@
 */
 import qr from "qr-image"
 // var qr = require('qr-image');
+import inquirer from 'inquirer';
+import fs from "fs" 
 
-var qr_img = qr.image("dd") 
-console.log(qr_img)
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'url',
+      message: 'Enter the URL you want to convert to QR code:'
+    }
+  ])
+  .then((answers) => {
+    // answers.url will contain the user's input
+    var qr_img = qr.image(answers.url)
+    fs.writeFile("image.txt", qr_img)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
