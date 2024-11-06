a89f08f4e6caba048c62b5b108df743e369ae20f
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-06 10:12:27 +0900

    mySol

--- Angela_course/2.3 NPM/index.js
+++ Angela_course/2.3 NPM/index.js
@@ -1,5 +1,10 @@
 import generateName from "sillyName"
+import {randomSuperhero} from 'superheroes';
+
 // var generateName = require("sillyname");
 var sillyName = generateName();
+var heroName = randomSuperhero();
 
-console.log(`My name ${sillyName}`);
+    console.log(`My name ${sillyName}`);
+    console.log(`My name ${heroName}`);
+
