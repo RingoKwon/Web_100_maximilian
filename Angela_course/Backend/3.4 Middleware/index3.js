42a88516f9e6dc29e847a6fa5c0b38c5d70797f8
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-07 16:46:32 +0900

    mySolution

--- Angela_course/Backend/3.4 Middleware/index3.js
+++ Angela_course/Backend/3.4 Middleware/index3.js
@@ -3,11 +3,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const logger = (req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+};
+
 app.use(logger);
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.post("/test", (req,res) => {
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
