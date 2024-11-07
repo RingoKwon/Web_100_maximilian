2edfaf12f1848668dc3fc449012f66527884a27a
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-07 16:48:06 +0900

    solution

--- Angela_course/Backend/3.4 Middleware/index3.js
+++ Angela_course/Backend/3.4 Middleware/index3.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-const logger = (req, res, next) => {
+function  logger (req, res, next) {
   console.log(req.url, req.method);
   next();
 };
