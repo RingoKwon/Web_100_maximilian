e3ec82d2256715e7738cc62b2f83463202e39a48
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-08 16:44:36 +0900

    dynamic

--- Angela_course/Backend/4.0.2 EJS/index.js
+++ Angela_course/Backend/4.0.2 EJS/index.js
@@ -4,9 +4,20 @@ const app = e();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  const d = new Date();
+  const weekOfDay = d.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (weekOfDay === 0 || weekOfDay === 6 || weekOfDay === 5) {
+    type = "a weekend";
+    adv = "have some fun";
+  }
+
   res.render("index.ejs", {
-    dayType: "a weekday",
-    advice: "it's time to work hard",
+    dayType: type,
+    advice: adv,
   });
 });
 
