c07f68504e6352512a080c71be54610fa305ec08
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-08 14:55:18 +0900

    cursor setting

--- Angela_course/Backend/4.0 EJS/index.js
+++ Angela_course/Backend/4.0 EJS/index.js
@@ -1,17 +1,25 @@
 import express from "express";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("solution", {
+        dayType: "Monday",
+        advice: "it's time to work hard"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
