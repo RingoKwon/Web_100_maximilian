7d47714c3d684b72c86f982c2321b5b27d68632f
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-08 11:38:45 +0900

    anglea solution

--- Angela_course/Backend/3.5 Secrets Project/index.js
+++ Angela_course/Backend/3.5 Secrets Project/index.js
@@ -11,21 +11,33 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-var pw; 
-
+var userIsAuth = false; 
+var pw;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function checkPw(req, res, next) {
+  const pw = req.body["password"];
+  if ( pw === "aa"){
+    userIsAuth = true ; 
+  }
+  next()
+
+}
+app.use(checkPw);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/check", (req, res) => {
-    pw = req.body["password"]; 
-    if(pw === "aa"){
-        res.sendFile(__dirname+"/public/secret.html")
-    }
-}); 
+  pw = req.body["password"];
+  if (userIsAuth) {
+    res.sendFile(__dirname + "/public/secret.html");
+  }else { 
+    res.sendFile(__dirname + "/public/index.html"); 
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
