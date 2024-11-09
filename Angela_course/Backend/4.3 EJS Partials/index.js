481cd29bdb5d400c7c01235c10af5fbf6678fe35
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-09 17:28:08 +0900

    Use static directory

--- Angela_course/Backend/4.3 EJS Partials/index.js
+++ Angela_course/Backend/4.3 EJS Partials/index.js
@@ -9,10 +9,15 @@ Step 2: Make sure that static files are linked to and the CSS shows up.
 Step 3: Add the routes to handle the render of the about and contact pages.
   Hint: Check the nav bar in the header.ejs to see the button hrefs
 Step 4: Add the partials to the about and contact pages to show the header and footer on those pages. */
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
