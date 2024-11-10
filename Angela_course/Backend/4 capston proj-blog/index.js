cdd9b333112c5edfda7b157cac513be7c02e801c
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-10 14:27:19 +0900

    header flex

---
+++ Angela_course/Backend/4 capston proj-blog/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
