241155c6a093badb4c630404f8f0428343c7dad8
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-11 15:34:26 +0900

    save

--- Angela_course/Backend/5.3 Axios/index.js
+++ Angela_course/Backend/5.3 Axios/index.js
@@ -15,8 +15,8 @@ app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result , error:null});
-    console.log('Response data:', result);
+    res.render("index.ejs", { data: result, error: null });
+    // console.log('Response data:', result);
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -26,9 +26,22 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
-  // var obj = JSON.parse(response)  
-
+  // console.log(req.body);
+  // var obj = JSON.parse(response)
+  try {
+    const type = req.body.type;
+    const participants = req.body.participants;
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+    );
+    const result = response.data;
+    console.log(result);
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
