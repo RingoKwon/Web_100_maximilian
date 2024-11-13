851ab4758d5120b080ef82141d6cf82d906f2b78
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-13 14:45:13 +0900

    axios post

--- Angela_course/Backend/5.5 REST APIs/index.js
+++ Angela_course/Backend/5.5 REST APIs/index.js
@@ -12,7 +12,7 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "f8ac9e1b-f443-4a02-bf93-b860212cdcc1";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
@@ -35,6 +35,23 @@ app.post("/get-secret", async (req, res) => {
 
 app.post("/post-secret", async (req, res) => {
   // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  const secret = req.body.secret;
+  const score = req.body.score;
+
+  try {
+    const result = await axios.post(
+      API_URL + "/secrets/" ,
+      {
+        secret: secret,
+        score: score
+      },
+      config
+    );
+    console.log(result);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
 app.post("/put-secret", async (req, res) => {
