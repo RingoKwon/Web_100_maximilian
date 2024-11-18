488f60e08f9e30ab99c04c91828190352bca7a85
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-18 15:01:53 +0900

    angela solution

--- Angela_course/Backend/6.0 DIY API/index.js
+++ Angela_course/Backend/6.0 DIY API/index.js
@@ -84,8 +84,13 @@ app.patch("/jokes/:id", (req, res) => {
 //7. DELETE Specific joke
 app.delete("/jokes/:id", (req, res) => {
   const id = parseInt(req.params.id);
-  jokes = jokes.filter(joke=> joke.id !== id )
-  res.json({message: `${id} deleted`})
+  const searchIdx = jokes.findIndex((joke) => joke.id === id);
+  if (searchIdx > -1) {
+    jokes.splice(searchIdx, 1);
+    res.sendStatus(200);
+  } else {
+    res.status(404).json({ error: `Joke with id: ${id} not found. No jokes were deleted` });
+  }
 });
 //8. DELETE All jokes
 
