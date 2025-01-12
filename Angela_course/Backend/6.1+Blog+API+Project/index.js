0980d3c6f880d04595f59fbfec7040a4947e1b9d
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-01-13 01:36:47 +0900

    ger specific post by id my solution

--- Angela_course/Backend/6.1+Blog+API+Project/index.js
+++ Angela_course/Backend/6.1+Blog+API+Project/index.js
@@ -47,6 +47,13 @@ app.get("/posts", (req, res) => {
 });
 
 //CHALLENGE 2: GET a specific post by id
+app.get("/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const findByFilter = (posts, id) => {
+    return posts.find((post) => post.id === id);
+  };
+  res.json(findByFilter(posts, id));
+});
 
 //CHALLENGE 3: POST a new post
 
