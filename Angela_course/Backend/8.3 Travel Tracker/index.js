1a7d2e1d4a975122b2ad3d77a2ca37016e808145
ringo <60028639+RingoKwon@users.noreply.github.com>
2025-01-14 10:59:24 +0900

    My solution

--- Angela_course/Backend/8.3 Travel Tracker/index.js
+++ Angela_course/Backend/8.3 Travel Tracker/index.js
@@ -17,20 +17,57 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const result = await db.query("select country_code from public.visited_countries")
 
-const total = result.rowCount
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query(
+    "select country_code from public.visited_countries"
+  );
   
-  const countries = result.rows.map(row => row.country_code);
-  console.log(countries)
+  const total = result.rowCount;
+  //Write your code here.
+
+  const countries = result.rows.map((row) => row.country_code);
+  console.log(countries);
 
   res.render("index.ejs", {
     countries: countries,
-    total: total
-  })
-  db.end();
+    total: total,
+  });
+});
+
+
+app.post("/add", async (req, res) => {
+  const countryName = req.body.country;
+  let countryCode;
+  try {
+    const result = await db.query(
+      `select country_code from public.countries where country_name = '${countryName}'`
+    );
+    countryCode = result.rows[0].country_code;
+  } catch (e) {
+    console.error("error", e);
+  }
+  console.log(countryCode);
+  console.log(countryName);
+  const query = {
+    text: "INSERT INTO visited_countries (country_code) VALUES($1)",
+    values: [countryCode],
+  };
+
+  try {
+    const result = await db.query(query);
+    console.log("Insert successful");
+  } catch (err) {
+    console.error("Error executing query", err);
+  }
+  res.redirect("/");
+});
+
+console.log("---"); /// dedugging
+
+// 서버 종료 시 데이터베이스 연결 종료
+process.on("exit", async () => {
+  await db.end();
 });
 
 app.listen(port, () => {
